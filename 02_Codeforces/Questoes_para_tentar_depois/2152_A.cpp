4af8742c7aac67e693e768c405d8d267b3c354b6
Sávio Cartaxo <savioferreirabeltrao1@gmail.com>
2025-12-14 16:26:15 -0300

    12/12/2024

---
+++ 02_Codeforces/Questoes_para_tentar_depois/2152_A.cpp
@@ -0,0 +1,23 @@
+// 12/12/2025
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t; cin >> t;
+
+    ll n;
+    while (t--) {
+        cin >> n;
+    
+        
+    }
+
+    return 0;
+}
