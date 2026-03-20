c7b43826820763a12054bc5d761f33431252841b
SavioCartaxo <savioferreirabeltrao1@gmail.com>
2026-03-20 11:19:48 -0300

    TEMPLATE C++

---
+++ Template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vl vector<ll>
+#define vll vector<vl>
+
+#define readll(x) ll x; cin >> x;
+#define readint(x) int x; cin >> x;
+#define reads(x) string x; cin >> x;
+#define readc(x) char x; cin >> x;
+
+#define endl "\n"
+#define pb push_back
+
+#define rep3(i,a,b) for (int i = a; i < b; i++)
+#define rep4(i,a,b,c) for (int i = a; i < b; i+=c)
+#define repneg3(i,a,b) for (int i = a; i > b; i--)
+#define repneg4(i,a,b,c) for (int i = a; i > b; i-=c)
+
+#define print(x) cout << x << endl;
+
+int main() {
+    print("Funcionou")
+    return 0;
+}
