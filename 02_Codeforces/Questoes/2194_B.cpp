96835c2dfb6aee7146b8642dfb46ceffe4acd8be
SavioCartaxo <savioferreirabeltrao1@gmail.com>
2026-05-14 23:14:18 -0300

    14/05/2026

---
+++ 02_Codeforces/Questoes/2194_B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vl vector<ll>
+#define vll vector<vl>
+#define vi vector<int>
+#define vii vector<vi>
+
+#define readll(x) ll x; cin >> x;
+#define readint(x) int x; cin >> x;
+#define reads(x) string x; cin >> x;
+
+#define endl "\n"
+#define pb push_back
+
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define repneg(i,a,b) for (int i = a; i > b; i--)
+
+#define print(x) cout << x << endl;
+
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+
+void solved() {
+    ll n, x, y; cin >> n >> x >> y;
+    vl v(n); for (auto &i : v) cin >> i;
+    
+    ll Max = v[0] - ((v[0] / x) * y);
+    ll soma = 0;
+    for (int i = 0; i < n; i++) {
+        ll aux = v[i] - (v[i] / x * y);
+    
+        if (aux > Max) {
+            Max = aux;
+        }
+        soma += (v[i] / x * y);
+    }
+
+    soma += Max;
+    print(soma)
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    readint(t)
+    while (t--) {
+        solved();
+    }
+    return 0;
+}
